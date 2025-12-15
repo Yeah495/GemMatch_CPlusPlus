dbc9e9d988132a0999b19365fcde5cffed1c5cd4
Justin <johndoe@example.comzhitongliu061115@gmail.com>
2025-12-15 23:32:36 +0800

    创建项目架构文件

---
+++ GemMatch/GameCore.cpp
@@ -0,0 +1,3 @@
+/*● GameCore.h (外观总管)
+  ○ 算法核心：处理消除后的下落填充。  
+  ○ 职责：Controller 层的唯一交互对象。它内部持有 Board、MatchFinder 和 GravitySystem，协调三者工作。*/
---
+++ GemMatch/GameCore.h
@@ -0,0 +1,4 @@
+#pragma once
+/*● GameCore.h (外观总管)
+  ○ 算法核心：处理消除后的下落填充。  
+  ○ 职责：Controller 层的唯一交互对象。它内部持有 Board、MatchFinder 和 GravitySystem，协调三者工作。*/
