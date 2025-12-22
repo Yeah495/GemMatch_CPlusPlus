961926d7bcce8e619862b5a6a8b81a29b4684b32
msy410 <2594396270@qq.com>
2025-12-22 20:36:48 +0800

    加了bgm

--- GemMatch/main.cpp
+++ GemMatch/main.cpp
@@ -1,6 +1,8 @@
 #include "MainWindow.h"
 #include "GameController.h"
 #include <QtWidgets/QApplication>
+#include <QMediaPlayer>
+#include <QAudioOutput>
 
 int main(int argc, char *argv[])
 {
@@ -10,6 +12,25 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.show();
 
+    // 设置并播放背景音乐（本地文件）
+    QMediaPlayer* bgPlayer = new QMediaPlayer(&a);
+    QAudioOutput* bgOutput = new QAudioOutput(&a);
+    bgPlayer->setAudioOutput(bgOutput);
+    bgOutput->setVolume(0.5); // 默认 50%
+    bgPlayer->setLoops(QMediaPlayer::Infinite);
+    bgPlayer->setSource(QUrl::fromLocalFile("D:/GemMatch_CPlusPlus/GemMatch/assets/sound/bgpiano.wav"));
+    bgPlayer->play();
+
+    // 退出时停止并释放（parent 为 QApplication 会在退出时删除）
+    QObject::connect(&a, &QApplication::aboutToQuit, [bgPlayer, bgOutput]() {
+        if (bgPlayer) {
+            bgPlayer->stop();
+            bgPlayer->setSource(QUrl());
+        }
+        if (bgOutput) {
+            bgOutput->setVolume(0.0);
+        }
+    });
 
     // 2. 创建控制器 (Controller) 并绑定 View
     // Controller 内部会自动创建 Model
@@ -19,5 +40,4 @@ int main(int argc, char *argv[])
     controller.startGame();
 
     return a.exec();
-    
 }
